55ea4dc523ba5454cef6cf949704031aa14c49cf
Semyon Okulov <67464545+scffs@users.noreply.github.com>
2024-11-11 13:49:28 +0700

    init

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace okulovSM;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace okulovSM;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
